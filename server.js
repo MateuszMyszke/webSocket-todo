bb25f1d3eda798ddd3f90766ce0dce57b590ac5c
MateuszMyszke <myszke93@gmail.com>
2023-06-15 10:01:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const socket = require('socket.io');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const tasks = [];
+
+app.use(cors());
+
+const server = app.listen(8000, () => {
+  console.log('Server is running...');
+});
+
+const io = socket(server);
+
+app.use((req, res) => {
+  res.status(404).send('404: Not found...');
+});
+
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected with ID:', socket.id);
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    console.log('Task added');
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (task) => {
+    tasks.splice(tasks.indexOf(task), 1);
+    socket.broadcast.emit('removeTask', task)
+  });
+});
+
+
+
+
